71578a8a8c857ff454973c3e0d5344f86a1d0c9e
Yuna <silvercherryblossoms0000@gmail.com>
2026-08-13 04:33:02 +0900

    Fix install script to work with POSIX sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,13 @@
+#!/bin/sh
+
+set -eu
+
 # Homebrew
 if ! command -v brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(brew shellenv)"
 
 # Packages
 #brew list zsh >/dev/null 2>&1 || brew install zsh
@@ -12,12 +16,12 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Login shell
 current_shell="$(dscl . -read /Users/$USER UserShell | awk '{print $2}')"
 
-if [[ "$current_shell" != "$(command -v zsh)" ]]; then
+if [ "$current_shell" != "$(command -v zsh)" ]; then
     chsh -s "$(command -v zsh)"
 fi
 
 # dein
-if [[ ! -d "$HOME/.cache/dein/repos/github.com/Shougo/dein.vim" ]]; then
+if [ ! -d "$HOME/.cache/dein/repos/github.com/Shougo/dein.vim" ]; then
     curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh \
         | sh /dev/stdin "$HOME/.cache/dein"
 fi
